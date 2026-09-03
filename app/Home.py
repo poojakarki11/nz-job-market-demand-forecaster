6e6de01a8d1d4ef0c3ba3962a09247aabac98f79
poojakarki11 <poojakarki0611@gmail.com>
2026-09-03 12:51:03 +1200

    Initial commit: NZ job market demand forecaster

---
+++ app/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="NZ Labour Demand Forecaster", layout="wide")
+
+st.title("NZ Labour Demand Forecaster")
+st.write(
+    "This app presents labour demand trends and short-term forecasts in New Zealand "
+    "using Jobs Online and official labour market data."
+)
+
+st.markdown("Use the sidebar to navigate between pages.")
